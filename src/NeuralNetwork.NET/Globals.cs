dc1b79fbcc2b7a3e889dccf349b6567e9c2ac3a9
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-31 23:57:16 +0100

    Global Random

---
+++ src/NeuralNetwork.NET/Globals.cs
@@ -0,0 +1,17 @@
+﻿global using static Globals;
+
+public static class Globals
+{
+    public static Random GlobalRandom { get; set; } = new();
+
+    public static double RandomDouble(double min, double max)
+    {
+        double range = max - min;
+        if (range <= 0)
+        {
+            throw new Exception("ERROR: max must be greater than min!");
+        }
+
+        return (GlobalRandom.NextDouble() * range) - min;
+    }
+}
