c300e738c360a8c8798f448c3d9120ab071db333
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-31 18:33:06 +0100

    Indexes

---
+++ src/NeuralNetwork.NET/Indexes/Indexes.md
@@ -0,0 +1,8 @@
+﻿Indexes.LayerIndex layerIndex = i;
+
+Indexes.LevelIndex levelIndex = (i, 0);
+Indexes.LevelIndex levelIndex = (i, layerIndex);
+
+Indexes.NeuronIndex neuronIndex = (i, 0, 0);
+Indexes.NeuronIndex neuronIndex = (i, (0, 0));
+Indexes.NeuronIndex neuronIndex = (i, levelIndex);
---
+++ src/NeuralNetwork.NET/Indexes/LayerIndex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.NET.Indexes;
+
+public struct LayerIndex : IEquatable<LayerIndex>
+{
+    public int Index { get; init; }
+
+    public LayerIndex(int index)
+    {
+        this.Index = index;
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? value) => (value is LayerIndex index) && (Index.Equals(index.Index) && Index.Equals(index.Index));
+
+    public bool Equals(LayerIndex other) => Index == other.Index;
+
+    public static implicit operator LayerIndex(int index) => new(index);
+}
