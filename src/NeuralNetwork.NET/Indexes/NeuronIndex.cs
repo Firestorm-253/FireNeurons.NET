c9f6577c42e8cb45ba5227d5a5e31ea08804792b
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-31 18:33:06 +0100

    Indexes

---
+++ src/NeuralNetwork.NET/Indexes/NeuronIndex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork.NET.Indexes;
+
+public struct NeuronIndex : IEquatable<NeuronIndex>
+{
+    public int Index { get; init; }
+    public LevelIndex LevelIndex { get; init; }
+
+    public NeuronIndex(int index, LevelIndex levelIndex)
+    {
+        this.Index = index;
+        this.LevelIndex = levelIndex;
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? value) => (value is NeuronIndex index) && (Index.Equals(index.Index) && LevelIndex.Equals(index.LevelIndex));
+
+    public bool Equals(NeuronIndex other) => Index == other.Index;
+
+    public static implicit operator NeuronIndex((int, LevelIndex) index) => new(index.Item1, index.Item2);
+    public static implicit operator NeuronIndex((int, int, LayerIndex) index) => new(index.Item1, (index.Item2, index.Item3));
+}
