bf1817d15d5ec729ffcd6ff59fd916c938a492ff
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-01-31 20:28:37 +0100

    Neuron

---
+++ src/NeuralNetwork.NET/Neuron.cs
@@ -0,0 +1,33 @@
+﻿using NeuralNetwork.NET.Indexes;
+
+namespace NeuralNetwork.NET;
+
+public class Neuron
+{
+    public NeuronIndex NeuronIndex { get; init; }
+    public Activation Activation { get; init; }
+
+    public double Value { get; set; }
+    public double Blank { get; set; }
+    
+    public Neuron(NeuronIndex neuronIndex, Activation activation)
+    {
+        this.NeuronIndex = neuronIndex;
+        this.Activation = activation;
+    }
+
+    public void CalculateValue()
+    {
+        double sum = 0;
+
+        // ToDo: sum all connections
+
+        this.Feed(sum);
+    }
+
+    public void Feed(double blank)
+    {
+        this.Blank = blank;
+        this.Value = this.Blank.Activate(this.Activation);
+    }
+}
