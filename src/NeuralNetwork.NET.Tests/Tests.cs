8ef4188a5d69c561481cd709d8e185d9134f812d
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-01 20:20:43 +0100

    Save- & Load-Test

--- src/NeuralNetwork.NET.Tests/Tests.cs
+++ src/NeuralNetwork.NET.Tests/Tests.cs
@@ -39,4 +39,39 @@ public class Tests
 
         var results = model.Evaluate(data, 3, 4);
     }
+
+    [TestMethod]
+    public void SaveLoad()
+    {
+        //# Seed Randomizer
+        GlobalRandom = new Random(randomSeed);
+
+        //# Initialize
+        var model = new NeuralNetwork();
+
+        //# InputLayers
+        model.Add(6, 0, Activation.Sigmoid);
+        model.Add(6, 1, Activation.Sigmoid);
+
+        //# HiddenLayers
+        model.Add(20, 2, Activation.LeakyRelu, 0, 1);
+        model.Add(20, 3, Activation.LeakyRelu, 2);
+
+        //# OutputLayers
+        model.Add(10, 4, Activation.Sigmoid, 3);
+
+        //# Compile
+        model.Randomize();
+
+        //# Test
+        const string name = "test-network";
+        model.Save(name, SaveType.Binary);
+        model.Save(name, SaveType.Json);
+
+        var binaryLoaded = new NeuralNetwork($"{name}.nn");
+        var jsonLoaded = new NeuralNetwork($"{name}.json");
+
+        Assert.AreEqual(binaryLoaded, jsonLoaded);
+        Assert.AreEqual(model, binaryLoaded);
+    }
 }
