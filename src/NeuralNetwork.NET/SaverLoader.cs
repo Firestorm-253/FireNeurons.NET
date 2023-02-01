be9614ad63b15fb5646cfbdab8102d4db4f769bf
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-01 15:19:12 +0100

    SaverLoader

---
+++ src/NeuralNetwork.NET/SaverLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+using NeuralNetwork.NET.Dto;
+
+namespace NeuralNetwork.NET;
+
+public partial class NeuralNetwork
+{
+    public void Save(string file)
+    {
+        var dto = new NeuralNetworkDto(this);
+        string json = JsonSerializer.Serialize(dto, new JsonSerializerOptions() { WriteIndented = false, });
+        File.WriteAllText(file, json);
+    }
+
+
+    public static NeuralNetwork Load(string file)
+    {
+        var dto = LoadFromJsonFile(file);
+
+        return null;
+    }
+
+    private static NeuralNetworkDto LoadFromJsonFile(string file)
+    {
+        string json = File.ReadAllText(file);
+        var model = JsonSerializer.Deserialize<NeuralNetworkDto>(json, new JsonSerializerOptions() { WriteIndented = false, });
+
+        if (model == null || json == "{}")
+        {
+            throw new Exception("ERROR: Loading failed!");
+        }
+        return model;
+    }
+}
