e95bc435c0ddb23209d07f61962b13e35ce54fbc
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-01 01:34:08 +0100

    NeuralNetwork
    - void Add(neurons, activation, layerIndex, inputlayerIndexes)
    - Layer Get(layerIndex)

---
+++ src/NeuralNetwork.NET/NeuralNetwork.cs
@@ -0,0 +1,38 @@
+﻿using NeuralNetwork.NET.Indexes;
+
+namespace NeuralNetwork.NET;
+
+public class NeuralNetwork
+{
+    private HashSet<Layer> Layers { get; init; } = null!;
+
+    public NeuralNetwork()
+    {
+        this.Layers = new HashSet<Layer>(new LayerEqualityComparer());
+    }
+
+    public void Add(int neurons,
+                    Activation activation,
+                    LayerIndex index,
+                    params LayerIndex[] inputs)
+    {
+        var layer = new Layer(index, neurons, activation);
+
+        foreach (var inputLayerIndex in inputs)
+        {
+            var inputLayer = this.Get(inputLayerIndex);
+            layer.Connect(inputLayer);
+        }
+        
+        if (this.Layers.Contains(layer))
+        {
+            throw new Exception("ERROR: LayerIndex must be unique!");
+        }
+        this.Layers.Add(layer);
+    }
+
+    public Layer Get(LayerIndex layerIndex)
+    {
+        return this.Layers.First(x => x.LayerIndex.Equals(layerIndex));
+    }
+}
