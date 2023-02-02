64dd34f6f4ecc536a046b533955a3258a74d0218
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-02 01:13:53 +0100

    Discount(this double)

--- src/NeuralNetwork.NET/Globals.cs
+++ src/NeuralNetwork.NET/Globals.cs
@@ -4,4 +4,9 @@ global using static NeuralNetwork.NET.Randoms;
 public static class Globals
 {
     public static Random GlobalRandom { get; set; } = new();
+
+    public static double Discount(this double oldValue, double newValue, double discountFactor)
+    {
+        return (oldValue * discountFactor) + ((1 - discountFactor) * newValue);
+    }
 }
