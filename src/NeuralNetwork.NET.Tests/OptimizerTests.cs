53801bd774ade4e904fca5bccebcd8b88f92c241
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-02-02 01:16:05 +0100

    Optimizer-Test Adam

---
+++ src/NeuralNetwork.NET.Tests/OptimizerTests.cs
@@ -0,0 +1,18 @@
+﻿using NeuralNetwork.NET.Optimizers;
+
+namespace NeuralNetwork.NET.Tests;
+
+[TestClass]
+public class OptimizerTests
+{
+    [TestMethod]
+    public void AdamTest()
+    {
+        var optimizer = new Adam();
+
+        var adamData = new AdamData() { Momentum = 0, MomentumSquared = 0, TimeStep = 1, Gradient = +0.6 };
+        optimizer.Calculate(adamData);
+
+        Assert.AreNotEqual(0, adamData.Delta);
+    }
+}
