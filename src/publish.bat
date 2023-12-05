755e4b305dfb521fa41964abbaf5b76698bbd7ce
Firestorm-253 <laurin.zehnder@hotmail.com>
2023-12-05 23:05:51 +0100

    FireNeurons.NET v1.2.2.6

--- src/publish.bat
+++ src/publish.bat
@@ -1,4 +1,4 @@
 dotnet build
 dotnet pack
-dotnet nuget push FireNeurons.NET\bin\Release\FireNeurons.NET.1.2.2.5.nupkg --skip-duplicate
+dotnet nuget push FireNeurons.NET\bin\Release\FireNeurons.NET.1.2.2.6.nupkg --skip-duplicate
 dotnet nuget push FireNeurons.NET.Analyser\bin\Release\FireNeurons.NET.Analyser.1.0.2.nupkg --skip-duplicate
